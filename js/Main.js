991aff1e71f983c18ba498f9c928710de6a6a437
lucas <lucascgsilva>
2019-06-16 10:10:46 -0300

    Update Main.js

--- js/Main.js
+++ js/Main.js
@@ -9,6 +9,22 @@ $(function() {
             for (i = 0, j = data.selected.length; i < j; i++) {
                 r.push(data.instance.get_node(data.selected[i]).text);
             }
-            $('#event_result').html('Selected: ' + r.join(', '));
+            const header = `<button onclick="clickInf()" id="inf">Informações</button>
+                            <button onclick="clickDir()" id="dir">Diretorios</button>`
+            const body = `Selected:  ${r.join(', ')}`
+            $('#event_header').html(header);
+            $('#event_body').html(body);
         })
 });
+//a
+//$('#event_result').html('Selected: ' + r.join(', '));
+
+function clickDir() {
+    const texto1 = `Diretorios`
+    $('#event_body').html(texto1);
+}
+
+function clickInf() {
+    const texto2 = `Teste!!!!!!!!!!!!!!`
+    $('#event_body').html(texto2);
+}
